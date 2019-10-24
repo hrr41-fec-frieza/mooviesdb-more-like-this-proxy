ae5f9db3d73c292dd0c721ecb8362aec501fc583
hay-0le <haley.olsen1@gmail.com>
2019-10-24 07:31:47 -0700

    Got proxy up and running, with three running services (will add 4th when it is ready). Still need to address styled component errors.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 4000;
+const port = 4001;
 const cors = require('cors');
 
 app.use(express.static(__dirname + '/../public/dist'));
 app.use(bodyParser.json());
-app.use(cors());
-app.use('/', proxy('http://localhost:3030'))
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-  if (err) {
-    console.log('Server error requesting home page: ', err)
-  } else {
-    res.send('Hello from the server')
-  }
-})
+app.use(cors());
+// app.use('/', proxy('http://localhost:4000'))
+
+
 
 app.listen(port, () => {
   console.log('Server listening on port: ', port)
